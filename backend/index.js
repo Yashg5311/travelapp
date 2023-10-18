6d8aa8c0e2726f78bb7cc922af38a4ea9140db2c
Yash Gaur <yashgaurcms@gmail.com>
2023-10-18 21:13:21 +0530

    lastfinal commit

--- backend/index.js
+++ backend/index.js
@@ -58,11 +58,13 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    // The path goes one level up from backend then into frontend/build
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
     });
 }
+
 
 
 app.listen(port, () => {
