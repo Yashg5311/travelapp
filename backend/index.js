b52791e400fb307cba556d52a4a6f274b68b1c87
Yash Gaur <yashgaurcms@gmail.com>
2023-10-18 20:50:23 +0530

    commit third

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
     });
